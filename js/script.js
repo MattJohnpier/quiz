7473c3b2f0aae6cfcb110ce5154d6f5c959517d8
matthew johnpier <mattjohnpier@gmail.com>
2018-12-12 23:15:25 +0000

    lone function

--- js/script.js
+++ js/script.js
@@ -2,13 +2,16 @@
 
 $(document).ready(function() {
     $("button").click(function() {
-        var name = $("#name").val();
+        var name = $("#confront").val();
         var techPlacement = "TBD";
         var imgSrc = "https://media.giphy.com/media/22zgHX8aop488/giphy.gif";
         var q1Result = $("#question1").val();
         var q2Result = $("#question2").val();
         var q3Result = $("#question3").val();
         var totalScore;
+        
     });
-
+    
+    function question1(confront)
+        
 });
