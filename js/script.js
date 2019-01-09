d7d352d6f14ae58c25ccb17c424d359cf15a1ea7
matthew johnpier <mattjohnpier@gmail.com>
2019-01-09 23:07:26 +0000

    make questions

--- js/script.js
+++ js/script.js
@@ -2,18 +2,24 @@
 
 $(document).ready(function() {
     $("button").click(function() {
-        var name = $("#confront").val();
-        var dndClass= "TBD";
+        var align= "TBD";
         var imgSrc = "https://media.giphy.com/media/22zgHX8aop488/giphy.gif";
         var q1Result = $("#question1").val();
         var q2Result = $("#question2").val();
         var q3Result = $("#question3").val();
-        var totalScore;
+        
+        var q1score = confrontscore(q1Result)
+        var q2score = dangerscore(q2Result)
+        var q3score = teamworkscore(q3Result)
+
+        var totalScore = allscore(q1score, q2score, q3score);
+        
+        $("#alignment").text(totalScore);
         
     });
     
     function confrontscore(answer) {
-        if (answer === " "){
+        if (answer === "dog"){
             return 1;
         }else if (answer === " "){
             return 2;
@@ -35,7 +41,7 @@ $(document).ready(function() {
             return 2;
         }else if (answer === " "){
             return 3;
-        }else if (answer === " "){
+        }else if (answer === "chair"){
             return 4;
         }else if (answer === " "){
             return 5;
@@ -50,12 +56,16 @@ $(document).ready(function() {
             return 2;
         }else if (answer === " "){
             return 3;
-        }else if (answer === " "){
+        }else if (answer === "javascript"){
             return 4;
         }else if (answer === " "){
             return 5;
         }else{
             return 0;
         }
+    }
+    
+    function allscore(confrontscore,teamworkscore,dangerscore) {
+        return confrontscore + dangerscore + teamworkscore
     }
 });
